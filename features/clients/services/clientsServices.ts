94066d7b8a8f9dbee5f93804d9d0195d40598e3d
AngelDevs <m.a.matosbaldera@gmail.com>
2022-10-20 18:03:19 -0400

    refactor: remove address feature logic

--- features/clients/services/clientsServices.ts
+++ features/clients/services/clientsServices.ts
@@ -1,21 +1,15 @@
-import { Address } from "../models/address";
 import { Client } from "../models/client";
 import { getClientById } from "./getClientById";
 import { getClients } from "./getClients";
-import { updateClientAddresses } from "./updateClientAddresses";
 
 export interface ClientServices {
   getClients: () => Promise<Client[]>;
   getClientById: (id: string) => Promise<Client | undefined>;
-  updateClientAddresses: (
-    client: Client,
-    addresses: Address[]
-  ) => Promise<Client>;
 }
 
 export const clientsServices: ClientServices = {
   getClients,
-  updateClientAddresses,
+
   getClientById,
 };
 
