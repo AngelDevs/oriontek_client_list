5047a24a12a9bfe1e551027b9efe23b2e6d6b8b8
AngelDevs <m.a.matosbaldera@gmail.com>
2022-10-20 12:22:32 -0400

    refactor: add get client by id to export and move get client local state to client services file

--- features/clients/services/clientsServices.ts
+++ features/clients/services/clientsServices.ts
@@ -1,10 +1,12 @@
 import { Address } from "../models/address";
 import { Client } from "../models/client";
+import { getClientById } from "./getClientById";
 import { getClients } from "./getClients";
 import { updateClientAddresses } from "./updateClientAddresses";
 
 export interface ClientServices {
   getClients: () => Promise<Client[]>;
+  getClientById: (id: string) => Promise<Client | undefined>;
   updateClientAddresses: (
     client: Client,
     addresses: Address[]
@@ -14,4 +16,11 @@ export interface ClientServices {
 export const clientsServices: ClientServices = {
   getClients,
   updateClientAddresses,
+  getClientById,
+};
+
+export const getClientsLocalState = () => {
+  const clientsString = localStorage.getItem("clients");
+  const clients = clientsString ? JSON.parse(clientsString) : [];
+  return clients as Client[];
 };
