6ebce255e950312b5ff2d6153f837db87967a193
AngelDevs <m.a.matosbaldera@gmail.com>
2022-10-20 12:25:36 -0400

    refactor: update client grid styles and logic

--- features/clients/ui/ClientsGrid/ClientsGrid.tsx
+++ features/clients/ui/ClientsGrid/ClientsGrid.tsx
@@ -3,19 +3,25 @@ import ClientCard from "./ClientCard";
 
 type Props = {
   clients: Client[];
+  onCardClick: (
+    e: React.MouseEvent<HTMLDivElement, MouseEvent>,
+    client: Client
+  ) => void;
 };
 
 const ClientsGrid = (props: Props) => {
-  const { clients } = props;
+  const { clients, onCardClick } = props;
 
   if (clients.length === 0) {
     return <div>No clients found</div>;
   }
 
   return (
-    <div className="p-4 grid grid-cols-fill-72 gap-8">
+    <div className="grid grid-cols-fill-72 gap-8">
       {clients.map((client) => {
-        return <ClientCard key={client.id} client={client} />;
+        return (
+          <ClientCard key={client.id} client={client} onClick={onCardClick} />
+        );
       })}
     </div>
   );
