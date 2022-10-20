056b9d617ec0d81f9b082bcb7129833f4d7af3ab
AngelDevs <m.a.matosbaldera@gmail.com>
2022-10-20 12:16:31 -0400

    feat: add default config to query client

--- features/core/config/client.ts
+++ features/core/config/client.ts
@@ -1,7 +1,14 @@
 import { QueryClient } from "@tanstack/react-query";
 
 // Create a client
-export const queryClient = new QueryClient();
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      staleTime: 1000 * 60 * 1, // 1 minute
+    },
+  },
+});
 
 export const apiClientKeys = {
   clients: "clients",
