735b3f86ba5b88adadd1c4099588e09c0aac6d8c
AngelDevs <m.a.matosbaldera@gmail.com>
2022-10-20 12:28:06 -0400

    feat: add main page structure and  components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,16 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { useGetClientsQuery } from "../features/clients/queries/useGetClientsQuery";
+import ClientsGrid from "../features/clients/ui/ClientsGrid/ClientsGrid";
+import Header from "../features/core/ui/layout/Header";
 
 const Home: NextPage = () => {
+  const router = useRouter();
+  const { isLoading, data } = useGetClientsQuery();
+
   return (
-    <div>
+    <div className="w-full">
       <Head>
         <title>Clients Manager</title>
         <meta
@@ -12,8 +19,20 @@ const Home: NextPage = () => {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Header />
 
-      <main></main>
+      <main className="p-4">
+        {isLoading ? (
+          <span>Loading...</span>
+        ) : (
+          <ClientsGrid
+            clients={data ?? []}
+            onCardClick={(e, client) => {
+              router.push(`/${client.id}`);
+            }}
+          />
+        )}
+      </main>
 
       <footer></footer>
     </div>
