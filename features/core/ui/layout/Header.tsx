a655ad31c36ffef0bdda3213676572ec7695888b
AngelDevs <m.a.matosbaldera@gmail.com>
2022-10-20 12:20:30 -0400

    refactor: update header component styles

--- features/core/ui/layout/Header.tsx
+++ features/core/ui/layout/Header.tsx
@@ -3,7 +3,7 @@ type Props = {};
 const Header = (props: Props) => {
   return (
     <header className="flex p-4 border-solid border-b border-b-white/50 w-full">
-      <span className=" italic font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+      <span className="text-xl text-gradient-primary italic font-bold">
         Client Manager
       </span>
     </header>
