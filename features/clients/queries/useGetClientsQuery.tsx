d3e8f599054502b8c8566de73e2dab41f963c5b3
AngelDevs <m.a.matosbaldera@gmail.com>
2022-10-20 12:24:46 -0400

    feat: create use get clients query

---
+++ features/clients/queries/useGetClientsQuery.tsx
@@ -0,0 +1,12 @@
+import { useQuery } from "@tanstack/react-query";
+import { apiClientKeys } from "../../core/config/client";
+import { clientsServices } from "../services/clientsServices";
+
+export const useGetClientsQuery = () => {
+  const { data, isLoading } = useQuery(
+    [apiClientKeys.clients],
+    clientsServices.getClients
+  );
+
+  return { isLoading, data };
+};
